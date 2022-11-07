5d2932d2f52a564ebaf92f3655079d1e8e6e3fc7
Arttutu <arthurtt08@gmail.com>
2022-11-07 18:04:42 -0300

    terminando a seção projetos e dinamizando o site

--- js/projeto.js
+++ js/projeto.js
@@ -1,28 +1,57 @@
-function creatProject(link, img, title, overview) {
-  console.log(link)
-  ;`
-    <div class="grid1">
+function creatProject(link, img, title, overview, codigo) {
+  return `
+  
         <div class="projeto__item">
-            <a ${link}">
-               <div class="projeto_${img}'"></div>
+            <a href=${link}>
+               <div class="projeto__${img}"></div>
             </a>
             <h3 class="projeto__nome">${title}</h3>
-             <p class="projeto__descricao">${overview}</p>
+            <p class="projeto__descricao">${overview}</p>
+             <a href ="${link}" class = "codigo"><button class = "codigo"> Código </button></a>
         </div>
   `
 }
 
-document.querySelector("#projetos").innerHTML = `
-  <h2 class="projetos__titulo">Projetos</h2>
-
-  <div class="projetos__itens">
-   ${creatProject(
-     "https://github.com/Arttutu/meu_portfolio",
-     "fotoSite",
-     "Site Portfólio",
-     "Site feito com HTML5, CSS3 e JavaScript."
-   )}
-  
-  </div>
-
-`
+document.querySelector(".grid").innerHTML =
+  creatProject(
+    "https://github.com/Arttutu/meu_portfolio",
+    "imagemSite",
+    "Site Portfólio",
+    "Site feito com HTML5, CSS3 e JavaScript. O desafio é mostrar meus conhecimentos e aprender, site está em constante mudança.",
+    "https://github.com/Arttutu/meu_portfolio"
+  ) +
+  creatProject(
+    "https://arttutu.github.io/formulario-js/",
+    "imagemvalidacaoJs",
+    "Formulário Javascript",
+    "Projeto de validação de dados de um formulário com  Javascript.",
+    "https://github.com/Arttutu/formulario-js"
+  ) +
+  creatProject(
+    "https://github.com/Arttutu/meu_portfolio",
+    "imagemFlutter",
+    "Aplicativo de Filme",
+    "Meu primeiro projeto em flutter. Um catálogo de filmes e séries a partir da API do TMDB",
+    "https://github.com/Arttutu/app_movies"
+  ) +
+  creatProject(
+    "arttutu.github.io/nlw-copa-do-mundo/",
+    "imagemNlw",
+    "Calendário Copa de Mundo 2022 Brasil",
+    "Site feito com HTML5, CSS3 e JavaScript. Calendário da Copa é um projeto que mostra os jogos da copa do mundo de 2022.",
+    "https://github.com/Arttutu/nlw-copa-do-mundo"
+  ) +
+  creatProject(
+    "https://arttutu.github.io/piano-virtual/",
+    "imagemPiano",
+    "Piano Virtual",
+    "Construção de um teclado virtual com JavaScript, desafio  aprender a trabalhar com eventos em JavaScript",
+    "https://github.com/Arttutu/piano-virtual"
+  ) +
+  creatProject(
+    "https://github.com/Arttutu/",
+    "imagemGithub",
+    "Outros Projetos",
+    "<strong>Aqui você pode encontrar outros projetos menores, feito na faculdade ou em cursos online</strong>",
+    "https://github.com/Arttutu/"
+  )
