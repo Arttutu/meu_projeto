59cf0f03dad09a468b776ef510aa94501bb1ff1c
Arthur <arthurtt08@gmail.com>
2022-11-26 11:06:27 -0300

    validacao

--- js/validacaoForm.js
+++ js/validacaoForm.js
@@ -1,54 +1,29 @@
 const form = document.getElementById("formulario")
 const campos = document.querySelectorAll(".required")
-const spans = document.querySelectorAll(".span-required")
-const icone = document.querySelectorAll(".icon")
-const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-
-//preve o clik no botao e para e executa as validações
-form.addEventListener("submit", (event) => {
-  event.preventDefault()
-  nameValidate()
-  sobreNomeValidate()
-  emailValidate()
-  senhaValidacao()
-})
+const span = document.querySelectorAll(".span-required")
+const email = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
 
 function setError(index) {
-  campos[index].style.border = "2px solid  hsl(0, 100%, 74%)"
-  spans[index].style.display = "block"
-  icone[index].style.display = "inline-block"
+  campos[index].style.border = "2px solid hsl(0, 100%, 74%)"
+  span[index].style.display = "block"
 }
-function removeError(index) {
-  campos[index].style.border = ""
-  spans[index].style.display = "none"
-  icone[index].style.display = "none"
+const removeError = (index) => {
+  campos[index].style.border = "none"
+  span[index].style.display = "none"
 }
-
 function nameValidate() {
-  if (campos[0].value.length == 0) {
+  if (campos[0].value.length < 3) {
     setError(0)
+    console.log("erro")
   } else {
     removeError(0)
   }
 }
-function sobreNomeValidate() {
-  if (campos[1].value.length < 3) {
+function emailValidate() {
+  if (!email.test(campos[1].value)) {
     setError(1)
+    console.log("erro email")
   } else {
     removeError(1)
-  }
-}
-function emailValidate() {
-  if (!emailRegex.test(campos[2].value)) {
-    setError(2)
-  } else {
-    removeError(2)
-  }
-}
-function senhaValidacao() {
-  if (campos[3].value.length < 8) {
-    setError(3)
-  } else {
-    removeError(3)
   }
 }
